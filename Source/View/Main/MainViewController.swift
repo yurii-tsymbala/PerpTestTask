452c137584b71e6bcfe5ec07c26d5f378be6ebab
Yurii Tsymbala <zumbala2013@gmail.com>
2019-01-17 13:28:34 +0200

    Update setup

---
+++ Source/View/Main/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  PerpTestTask
+//
+//  Created by Yurii Tsymbala on 1/17/19.
+//  Copyright © 2019 Yurii Tsymbala. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
