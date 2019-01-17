ae5dd25b07358624775fbc06da083adb5b307632
Yurii Tsymbala <zumbala2013@gmail.com>
2019-01-17 13:41:17 +0200

    Updated Service

--- Source/Networking/DownloadService.swift
+++ Source/Networking/DownloadService.swift
@@ -8,3 +8,28 @@
 
 import Foundation
 
+enum DownloadServiceError {
+
+}
+
+protocol DownloadServiceType {
+  typealias DownloadHandler = (Result<[], DownloadServiceError>) -> Void
+  func fetchDataFromFile(completion: @escaping DownloadHandler)
+}
+
+class DownloadService: DownloadServiceType {
+
+  func fetchDataFromFile(completion: @escaping DownloadHandler) {
+    let textURL = URL(string: "https://www.metoffice.gov.uk/pub/data/weather/uk/climate/stationdata/bradforddata.txt")!
+    URLSession.shared.downloadTask(with: textURL) { [weak self]  (dataResponse,_,error) in
+      guard let strongSelf = self else {return}
+      guard error == nil else {completion();return}
+      guard let dataResponse = dataResponse else { completion(Result.failure(); return }
+      do {
+      } catch _ {
+        completion()
+      }
+      }.resume()
+}
+}
+
