765253e1344797084817a4113640c800b61c2234
Yurii Tsymbala <zumbala2013@gmail.com>
2019-01-17 13:14:45 +0200

    First Setup

---
+++ PerpTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PerpTestTask
+//
+//  Created by Yurii Tsymbala on 1/17/19.
+//  Copyright © 2019 Yurii Tsymbala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
